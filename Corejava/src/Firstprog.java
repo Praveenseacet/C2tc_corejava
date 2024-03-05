82a66bbbea4a97da0a0d3478655483141076cc14
Admin <Admin@DESKTOP-KOLRH8F>
2024-03-05 12:01:26 -0500

    First commit

---
+++ Corejava/src/Firstprog.java
@@ -0,0 +1,13 @@
+
+public class Firstprog {
+
+	public Firstprog() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+
+}
