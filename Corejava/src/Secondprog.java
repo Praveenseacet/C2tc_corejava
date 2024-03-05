74af806bd445f10f47f2d4c1f7faa58af70aa45d
Admin <Admin@DESKTOP-KOLRH8F>
2024-03-05 12:14:58 -0500

    First commit

---
+++ Corejava/src/Secondprog.java
@@ -0,0 +1,13 @@
+
+public class Secondprog {
+
+	public Secondprog() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method s
+		System.out.println("SEACET");
+	}
+
+}
